a13d623a089bde7ceacd0b0f7a1f4f57faffd003
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-08-21 01:33:56 +0530

    JS

---
+++ main.js
@@ -0,0 +1,12 @@
+var dateOfBirth = document.querySelector("#date-of-birth"); 
+var luckyNumber = document.querySelector("#lucky-number");
+var checkButton = document.querySelector("#birthday-lucky");
+
+dateOfBirth = dateOfBirth.replaceAll('-',' ');
+function sumOfDigits(){
+    
+}
+
+checkButton.addEventListener("click", function checkBirthdayLucky(){
+    console.log("clicked")
+})
