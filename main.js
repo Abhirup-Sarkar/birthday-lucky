b32fc502139758b37574ff5a27fb4b8e6588540a
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-08-25 22:22:01 +0530

    JS logic done

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 var dateOfBirth = document.querySelector("#date-of-birth");
 var luckyNumber = document.querySelector("#lucky-number");
 var checkButton = document.querySelector("#birthday-lucky");
+var outputBox = document.querySelector("#output-text")
 
 
-function checkBirthdayIsLucky() {
-    var dob = dateOfBirth.value;
-    dob = dob.replaceAll("-", "");
-    var sum = sumOfDigits(dob);
-    
-    
-    
+
+function checkValues(sum,lucky){
+    if(sum%lucky == 0){
+        outputBox.innerText="Your Birthday is lucky";
+    }
+    else{
+        outputBox.innerText="Your birthday is not lucky";
+    }
 }
 function sumOfDigits(dob){
-    s = 0;
-    while (dob>0){
-        
-        s = s + dob % 10;
-        dob = parseInt(dob / 10);
+    dob = dob.replaceAll("-", "");
+    sum = 0;
+    while(dob!=0){
+        sum = sum + (dob%10);
+        dob = parseInt(dob/10)
     }
-    console.log(s)
+    return sum
 }
-
-
-
+function checkBirthdayIsLucky() {
+    var dob = dateOfBirth.value;
+    var sum = sumOfDigits(dob);
+    var lucky = luckyNumber.value;
+   
+    checkValues(sum,lucky)
+    
+    
+}
 checkButton.addEventListener("click", checkBirthdayIsLucky);
